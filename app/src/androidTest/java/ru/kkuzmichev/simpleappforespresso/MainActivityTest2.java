5bf2333cdbb2523fe3140b59256d01bd2e5c2df6
AsjaMedved <anastasjamedved@gmail.com>
2025-11-16 19:01:16 +0300

    добавление MainActivityTest2

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest2.java
@@ -0,0 +1,74 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static ru.kkuzmichev.simpleappforespresso.utils.TestUtils.childAtPosition;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.not;
+
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.espresso.intent.Intents;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testOpenSettingsAndIntent() {
+        intending(not(isInternal()))
+                .respondWith(new Instrumentation.ActivityResult(0, null));
+
+        onView(allOf(
+                withContentDescription("More options"),
+                childAtPosition(
+                        childAtPosition(withId(R.id.toolbar), 2),
+                        0),
+                isDisplayed()))
+                .perform(click());
+
+        onView(allOf(withText("Settings"), isDisplayed()))
+                .perform(click());
+
+        intended(allOf(
+                hasAction(Intent.ACTION_VIEW),
+                hasData(Uri.parse("https://google.com"))
+        ));
+    }
+}
